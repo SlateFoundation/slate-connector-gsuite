0c7b443376b3b6c50593607145eb6b373b39ae7f
Nafis Bey <nafis@jarv.us>
2020-04-02 21:42:35 +0000

    fix: remove keys from gsuite config

---
+++ php-config/Slate/Connectors/GSuite/API.config.d/credentials.php
@@ -0,0 +1,10 @@
+<?php
+
+use Slate\Connectors\GSuite\API;
+
+// Required For Service Account Authentication with Domain wide Delegation
+// https://developers.google.com/admin-sdk/directory/v1/guides/delegation
+// API::$clientId = 'client-id';
+// API::$clientEmail = 'service-account-email@gserviceaccount.com';
+// API::$privateKey = 'private-rsa-key';
+// API::$domain = 'example.org';
