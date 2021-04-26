678551505dc118271e9d3114c29c1bff895b1613
Chris Alfano <chris@jarv.us>
2021-04-26 14:52:29 +0000

    fix(connectors/google-apps): reclassify unmapped existing user as a skip

--- php-classes/Slate/Connectors/GSuite/Connector.php
+++ php-classes/Slate/Connectors/GSuite/Connector.php
@@ -160,10 +160,10 @@ class Connector extends AbstractConnector implements ISynchronize
             // update existing remote user
             if ($googleUser) {
                 if (!$DomainEmailPoint) {
-                    $Job->error('Cannot update existing remote user {username} because they don\'t have an email contact point matching the domain', [
+                    $Job->debug('Cannot update existing remote user {username} because they don\'t have an email contact point matching the domain', [
                         'username' => $User->Username
                     ]);
-                    $results['outcome']['failed']['no-domain-email-contact-point']++;
+                    $results['outcome']['skipped']['existing-user-no-domain-email-contact-point']++;
                     continue;
                 }
 
